ccd4ec50a77be64a99126398157de287154674cf
Trong Hoang <hoangngotrong2004@gmail.com>
2025-04-18 02:30:37 +0700

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,28 @@
+import './Login.scss';
+
 const Login = (props) => {
     return (
-        <div>
-            <h1>Login</h1>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
+        <div className="login-container ">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block ">
+                        <div className='brand'>Trong Hoang</div>
+                        <div className='detail'>abc</div>
+                    </div>
+
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3 ">
+                        <input type='text' placeholder='Username' className='input' />
+                        <input type='password' placeholder='Password' className='input' />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'><a href='#' className='forgot-pass'>Forgot password</a></span>
+                        <hr />
+                        <div className='text-center'><button className='btn btn-success'>Create account</button></div>
+
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
